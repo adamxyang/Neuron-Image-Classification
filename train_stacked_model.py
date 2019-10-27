9a7c1273b43a7bbe2677253383be34a30e2f2055
Adam Yang <adamxinyuyang@icloud.com>
2019-10-27 20:54:28 +0000

    Update train_stacked_model.py

--- train_stacked_model.py
+++ train_stacked_model.py
@@ -212,19 +212,9 @@ def get_loader(drug_list):
     sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
     seq = iaa.Sequential([
-#             iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
+            iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
             iaa.Fliplr(0.5), # horizontally flip 50% of the images
             iaa.Flipud(0.2), # vertically flip 20% of all images
-#             sometimes(iaa.Affine(
-#                 scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
-#                 translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-#                 rotate=(-45, 45), # rotate by -45 to +45 degrees
-# #                 shear=(-16, 16), # shear by -16 to +16 degrees
-# #                 order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
-# #                 cval=(0, 255), # if mode is constant, use a cval between 0 and 255
-# #                 mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
-#             )),
-#             iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
             ])
     
     df_new = pd.read_csv('/mnt/sdh/adam/test/df_new.csv')
